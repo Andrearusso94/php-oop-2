d032cc6134ab07b366545580b4338029aedf8eb6
Andrearusso94 <andrea.r.4845@gmail.com>
2022-12-12 16:16:35 +0100

    add card

--- Models/db.php
+++ Models/db.php
@@ -4,19 +4,22 @@ require __DIR__ . '/Product.php';
 
 
 
-$product_1 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg');
+$product_1 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), './img/dog.jpg');
 $product_2 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg');
 $product_3 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg');
 $product_4 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg');
 $product_5 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg');
 $product_6 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg');
+$product_7 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg');
+$product_8 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg');
 
 $db = [
     new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg'),
     new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg'),
     new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg'),
+    new Product('Lettiera', new Type('Accessorio ',  ' Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg'),
+    new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg'),
     new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg'),
     new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg'),
-    new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg')
-
+    new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg'),
 ];
--- index.php
+++ index.php
@@ -26,28 +26,33 @@ require __DIR__ . '/Models/db.php';
 
 </head>
 
-<body>
+<body class="bg-secondary">
     <header>
         <!-- place navbar here -->
     </header>
     <main>
-        <div class="card" style="width: 18rem;">
-            <?php foreach ($db as $product) : ?>
-                <img class="card-img-top" src="<?= $product->image ?>" alt="Card image cap">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Cras justo odio</li>
-                    <li class="list-group-item">Dapibus ac facilisis in</li>
-                    <li class="list-group-item">Vestibulum at eros</li>
-                </ul>
-                <div class="card-body">
-                    <a href="#" class="card-link">Card link</a>
-                    <a href="#" class="card-link">Another link</a>
-                </div>
-            <?php endforeach; ?>
+        <div class="container d-flex">
+            <div class="row">
+                <?php foreach ($db as $product) : ?>
+                    <div class="card mx-3 my-3" style="width: 18rem;">
+
+                        <img class="card-img-top" src="<?= $product->image ?>" alt="Card image cap">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $product->name ?></h5>
+                            <p class="card-text"><?= $product->type->name ?></p>
+                            <p class="card-text"><?= $product->type->type_food ?></p>
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item"><?= $product->categories->name ?></li>
+                            <li class="list-group-item"><?= $product->price ?></li>
+
+                        </ul>
+
+
+                    </div>
+                <?php endforeach; ?>
+            </div>
+
         </div>
     </main>
     <footer>
