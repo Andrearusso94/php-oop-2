a9ad5e04e67f36d84d32e9cb859baa0520a350c0
Andrearusso94 <andrea.r.4845@gmail.com>
2022-12-12 16:34:06 +0100

    add end ex and initial bonus

--- Models/db.php
+++ Models/db.php
@@ -5,21 +5,21 @@ require __DIR__ . '/Product.php';
 
 
 $product_1 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), './img/dog.jpg');
-$product_2 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg');
-$product_3 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg');
-$product_4 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg');
-$product_5 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg');
-$product_6 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg');
-$product_7 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg');
-$product_8 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg');
+$product_2 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), './img/cat.webp');
+$product_3 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), './img/dog.jpg');
+$product_4 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), './img/cat.webp');
+$product_5 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), './img/dog.jpg');
+$product_6 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), './img/cat.webp');
+$product_7 = new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), './img/dog.jpg');
+$product_8 = new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), './img/cat.webp');
 
 $db = [
-    new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg'),
-    new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg'),
-    new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg'),
-    new Product('Lettiera', new Type('Accessorio ',  ' Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg'),
-    new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg'),
-    new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg'),
-    new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), '/img/dog.jpg'),
-    new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), 'urlimg'),
+    new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), './img/dog.jpg'),
+    new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), './img/cat.webp'),
+    new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), './img/dog.jpg'),
+    new Product('Lettiera', new Type('Accessorio ',  ' Materiale non commestibile'), 25, new Categories('Gatto'), './img/cat.webp'),
+    new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), './img/dog.jpg'),
+    new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), './img/cat.webp'),
+    new Product('Croccantini', new Type('Cibo', 'Manzo/Zucchine'), 25, new Categories('Cane'), './img/dog.jpg'),
+    new Product('Lettiera', new Type('Accessorio', 'Materiale non commestibile'), 25, new Categories('Gatto'), './img/cat.webp'),
 ];
